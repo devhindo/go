02d75cec26d79150ff3ddcae6e5c55fd5df536ab
hindawi <ahmedhindawi2001@gmail.com>
2023-07-06 05:57:40 +0300

    added some code

---
+++ projects/little-go-book/import.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		os.Exit(1)
+	}
+	fmt.Println("it's over", os.Args[1], "num of args: ", len(os.Args))
+	fmt.Println(reflect.TypeOf(os.Args))
+	fmt.Println(os.Args[0])
+}

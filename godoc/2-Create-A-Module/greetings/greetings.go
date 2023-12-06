5ec6758fcaebd93177ba31dd7b61bed555377239
devhindo <lildevhind@gmail.com>
2023-12-06 13:10:07 +0200

    finished create module

--- godoc/2-Create-A-Module/greetings/greetings.go
+++ godoc/2-Create-A-Module/greetings/greetings.go
@@ -12,7 +12,8 @@ func Hello(name string) (string, error) {
     if name == "" {
         return "", errors.New("empty name")
     }
-    message := fmt.Sprintf(randomFormat(), name)
+    // message := fmt.Sprintf(randomFormat(), name)
+    message := fmt.Sprint(randomFormat())
     return message, nil
 }
 
